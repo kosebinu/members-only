795a00e531d9d1a17fddfb3862fdfe6764b1e207
Kazeem Kosebinu <138680238+kosebinu@users.noreply.github.com>
2024-12-27 18:47:11 -0500

    Update database

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     user: "kosebinu",    
+//     host: "localhost",  
+//     database: "clubhouse", 
+//     password: "kosebinu",   
+//     port: 5432,  
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "kosebinu",    
-    host: "localhost",  
-    database: "clubhouse", 
-    password: "kosebinu",   
-    port: 5432,  
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
