f0418db05735ad89e157aa9e265a05c95a1f6a13
Kazeem Kosebinu <138680238+kosebinu@users.noreply.github.com>
2024-12-27 19:14:14 -0500

    Revert to localhost

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//     user: "kosebinu",    
-//     host: "localhost",  
-//     database: "clubhouse", 
-//     password: "kosebinu",   
-//     port: 5432,  
-// });
-
-// module.exports = pool;
-
-
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    user: "kosebinu",    
+    host: "localhost",  
+    database: "clubhouse", 
+    password: "kosebinu",   
+    port: 5432,  
 });
 
 module.exports = pool;
+
+
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+// });
+
+// module.exports = pool;
